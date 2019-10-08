ef032718e4a7985fe59708518f80cde160a297c6
YasasDEK <ydilshan.ek@gmail.com>
2019-10-08 12:42:13 +0530

    new about

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,45 +1,46 @@
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Component, OnInit, Renderer } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
     selector: 'app-components',
     templateUrl: './components.component.html',
-    styles: [`
-    ngb-progressbar {
-        margin-top: 5rem;
-    }
-    `]
+    styles: ['./components.component.css']
 })
 
 export class ComponentsComponent implements OnInit {
-    page = 4;
-    page1 = 5;
-    focus;
-    focus1;
-    focus2;
-    date: {year: number, month: number};
-    model: NgbDateStruct;
-    constructor( private renderer : Renderer) {}
-    isWeekend(date: NgbDateStruct) {
-        const d = new Date(date.year, date.month - 1, date.day);
-        return d.getDay() === 0 || d.getDay() === 6;
-    }
+    searchterm: string;
 
-    isDisabled(date: NgbDateStruct, current: {month: number}) {
-        return date.month !== current.month;
-    }
+    startAt = new Subject();
+    endAt =  new Subject();
+
+    jobs;
+    alljobs
+
+    startobs = this.startAt.asObservable;
+    endobs = this.endAt.asObservable;
+
+    constructor(private afs: AngularFirestore) {}
 
     ngOnInit() {
-        let input_group_focus = document.getElementsByClassName('form-control');
-        let input_group = document.getElementsByClassName('input-group');
-        for (let i = 0; i < input_group.length; i++) {
-            input_group[i].children[0].addEventListener('focus', function (){
-                input_group[i].classList.add('input-group-focus');
-            });
-            input_group[i].children[0].addEventListener('blur', function (){
-                input_group[i].classList.remove('input-group-focus');
-            });
-        }
+        Observable.combineLatest(this.startobs , this.endobs).subscribe((value) => {
+            this.firequery(value[0], value[1]).subscribe((jobs) => {
+                this.alljobs = jobs;
+            })
+        })
+    }
+
+    search(event) {
+        let q = event.target.value;
+        this.startAt.next(q);
+        this.endAt.next(q + '\uf8ff');
+    }
+
+    firequery(start, end) {
+        return this.afs.collection('signup', ref => ref.limit(4).orderBy('job').startAt(start).endAt(end)).valueChanges();
     }
 
 }
+
+
