b6a4e00bf359d9f9dd7a27eb5a127d00e590e30d
Ranindu Jaysooriya <43095375+Ranindu15@users.noreply.github.com>
2019-10-17 21:13:52 +0530

    Add files via upload

---
+++ src/app/examples/services/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    uid: string;
+    username: string;
+    idNumber: string;
+    email: string;
+    emailVerified: boolean;
+    jobType: string;
+    mobile: string;
+}
