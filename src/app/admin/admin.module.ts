06a94ac5002dd56ba496f57a418875eee2bb937d
YasasDEK <ydilshan.ek@gmail.com>
2020-01-10 16:11:23 +0530

    message created

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,6 +11,7 @@ import { AddshopsComponent } from './addshops/addshops.component';
 import { AddcompaniesComponent } from './addcompanies/addcompanies.component';
 import { VerifyWorkersComponent } from './verify-workers/verify-workers.component';
 import { EditAdminProfileComponent } from './edit-admin-profile/edit-admin-profile.component';
+import { ViewMessagesComponent } from './view-messages/view-messages.component';
 // import { SignUpComponent } from './sign-up/sign-up.component';
 // import { SignInComponent } from './sign-in/sign-in.component';
 
@@ -31,6 +32,7 @@ import { EditAdminProfileComponent } from './edit-admin-profile/edit-admin-profi
         AddcompaniesComponent,
         VerifyWorkersComponent,
         EditAdminProfileComponent,
+        ViewMessagesComponent,
         // ReglogComponent,
         // SignInComponent
     ]
